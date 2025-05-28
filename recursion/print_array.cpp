7f00dd7f9beec8cc92de848c1f707775d6b124d7
vivek <codecrafter67@gmail.compwd>
2025-05-28 14:15:35 +0530

    add print_array.cpp to implement recursive printing of array elements

---
+++ recursion/print_array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void printArrayElements(int arr[],int idx,int size){
+    if (idx==size) return;
+
+    cout << arr[idx] << " " << endl;
+    printArrayElements(arr,idx+1,size);
+}
+
+int main(){
+
+    int arr[10] = {10,20,30,40,50,60,70,80,90,100};
+    int size = 10;
+    printArrayElements(arr,4,size);
+}
